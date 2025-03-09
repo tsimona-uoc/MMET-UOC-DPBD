ed6a1065ad6e83280ee664fa662a653ac389b150
tsimona-uoc <tsimona@uoc.edu>
2025-03-09 16:26:18 +0100

    Pregunta 1.7 - agregadas consultas para agregar campos
    Se agregan los campos enddate y birthdate y las consultas que aplican esos cambios a la BBDD.

--- FP.046_Pn_Cnv[mmaa]_Grupo[num].sql
+++ FP.046_Pn_Cnv[mmaa]_Grupo[num].sql
@@ -58,10 +58,22 @@ ADD COLUMN birthdate DATE;
 -- Pregunta 1.6 Revisar los comentarios en las tablas y cambiar los campos que así lo requieran, por campos autocalculados.
 
 -- Pregunta 1.7 Agregar dos campos adicionales a la Base de Datos que enriquezca la información de la misma. Justificar.
+alter table `event` add column `enddate` DATETIME DEFAULT NULL;
+/*
+* Agregamos el campo enddate de tipo DATETIME nulable, en la tabla event. Esto permitira guardar el instante en el que se finalizó el evento.
+* Posible uso: Analisis de datos y optimización de espacio de tiempo reservado de futuros eventos (o estimación de la duración)
+*/
+alter table `users` add column birthdate DATETIME DEFAULT NULL AFTER phone;
+/*
+* Agregamos el campo birthdate de tipo DATETIME (por defecto NULL ya que no se dispone de las fechas de nacimiento) justo antes de la columna "phone".
+* Posible uso: Analisis de datos, comprobar el publico objetivo de cada evento por edades.
+*/
 
 -- Pregunta 1.8 Crear un disparador que al actualizar el campo username de la tabla users revise si su contenido contiene mayúsculas, minúsculas, digitos y alguno de los siguientes símbolos: -_#@. De no ser así, no permitir la actualización.
 
+
 -- Pregunta 1.9 Diseñar un disparador que prevenga que el campo email de la tabla users tenga un formato correcto al actualizar o insertar un nuevo email.
+
 
 -- Pregunta 1.10 Inventar una restricción que sirva de utilidad para mantener la integridad de la Base de Datos.
 
