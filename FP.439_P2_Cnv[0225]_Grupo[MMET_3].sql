1a833e810560b3e5adc7901fd2a8f2aaba781d55
rusterboy77 <37505980+rusterboy77@users.noreply.github.com>
2025-04-01 12:07:06 +0200

    Finalizado Combinaciones externas

--- FP.439_P2_Cnv[0225]_Grupo[MMET_3].sql
+++ FP.439_P2_Cnv[0225]_Grupo[MMET_3].sql
@@ -34,7 +34,8 @@ FROM users
 LEFT JOIN sales ON users.userid = sales.buyerid
 WHERE sales.buyerid IS NULL;
 
--- Pregunta 2.15 Crear una nueva tabla usuarios sin ventas que deberá guardar aquellos usuarios que no han vendido ningún ticket con los campos userid, firstname, lastname, phone.
+-- Pregunta 2.15 Crear una nueva tabla usuarios sin ventas que deberá guardar aquellos usuarios que no han vendido ningún ticket
+-- con los campos userid, firstname, lastname, phone.
 
 /* Creamos la tabla de usuarios sin ventas*/
 
@@ -51,18 +52,58 @@ INSERT INTO users_without_sells (userid, firstname, lastname, phone)
 SELECT users.userid, users.firstname, users.lastname, users.phone
 FROM users
 LEFT JOIN sales ON users.userid = sales.sellerid
-WHERE sales.buyerid IS NULL;
+WHERE sales.sellerid IS NULL;
 
 
 -- Pregunta 2.16 Mostrar una lista con todos los usuarios que no se encuentren en la tabla listing con los campos userid, firstname, lastname, phone.
 
+/* Seleccionamos los campos que deseamos mostrar de la tabla users y lo comparamos con la tabla listing mediante LEFT JOIN
+   al igual que en los casos anteriores. Filtramos mediante WHERE .... IS NULL aquellos usuarios que no tienen registro en la tabla listing*/
+
+SELECT users.userid, users.firstname, users.lastname, users.phone
+FROM users
+LEFT JOIN listing ON users.userid = listing.sellerid
+WHERE listing.sellerid IS NULL;
 
 -- Pregunta 2.17 Mostrar aquellas fechas en las cuales no ha habido ningún evento. Se deberá mostrar los campos caldate y holiday
+
+SELECT date.caldate, date.holiday
+FROM date
+LEFT JOIN event ON date.dateid = event.dateid
+WHERE event.dateid IS NULL; /* No devuelve ninguna fecha ya que en todas las fechas hubo algun evento */
 
 -- Resolver con Subconsultas
 
 -- Pregunta 2.18 Mostrar la cantidad de tickets vendidos y sin vender para las diferentes categorías de eventos.
--- Pregunta 2.19 Crea una consulta que calcule el precio promedio pagado por venta y la compare con el precio promedio por venta por trimestre. La consulta deberá mostrar tres campos: trimestre, precio_promedio_por_trimestre, precio_promedio_total
+
+SELECT category.catid, category.catname,
+    (SELECT SUM(sales.qtysold) /* Sumamos todos los tickets vendidos de la tabla sales */
+     FROM sales 
+     WHERE sales.eventid IN ( /* Filtramos mediante una subconsulta los tickets vendidos en la tabla sales cuyos valores coinciden
+                                 en la tabla eventos mediante catid */
+         SELECT event.eventid 
+         FROM event 
+         WHERE event.catid = category.catid)) AS tickets_vendidos, /* Mostramos el resultado como tickets vendidos */
+    (SELECT SUM(listing.numtickets) /* Sumamos el total de tickets disponibles para la venta */
+     FROM listing 
+     WHERE listing.eventid IN ( /* Filtramos mediante una subconsulta los tickets totales en la tabla listing cuyos valores coinciden
+                                 en la tabla eventos mediante catid */
+         SELECT event.eventid 
+         FROM event 
+         WHERE event.catid = category.catid)) 
+    -  /* Restamos los tickets vendidos del total de tickets disponibles para obtener el total de tickets no vendidos */
+    (SELECT SUM(sales.qtysold) 
+     FROM sales 
+     WHERE sales.eventid IN (
+         SELECT event.eventid 
+         FROM event 
+         WHERE event.catid = category.catid)) AS tickets_no_vendidos
+FROM category;
+
+-- Pregunta 2.19 Crea una consulta que calcule el precio promedio pagado por venta y la compare con el precio promedio por venta por 
+-- trimestre. La consulta deberá mostrar tres campos: trimestre, precio_promedio_por_trimestre, precio_promedio_total
+
+
 -- Pregunta 2.20 Muestra el total de tickets de entradas compradas de Shows y Conciertos.
 -- Pregunta 2.21 Muestra el id, fecha, nombre del evento y localización del evento que más entradas ha vendido.
 
