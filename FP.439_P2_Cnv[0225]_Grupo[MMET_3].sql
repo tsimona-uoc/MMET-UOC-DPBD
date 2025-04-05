11db8c2dbc3a3a4832508fab53af8ef9ed4b70c8
tsimona-uoc <tsimona@uoc.edu>
2025-04-05 13:05:32 +0200

    Agregados los ejericios de Evarishtu desde su rama

--- FP.439_P2_Cnv[0225]_Grupo[MMET_3].sql
+++ FP.439_P2_Cnv[0225]_Grupo[MMET_3].sql
@@ -75,9 +75,45 @@ WHERE event.dateid IS NULL; /* No devuelve ninguna fecha ya que en todas las fec
 -- Resolver con Subconsultas
 
 -- Pregunta 2.18 Mostrar la cantidad de tickets vendidos y sin vender para las diferentes categorías de eventos.
+SELECT c.catgroup, 
+COALESCE (SUM(s.qtysold), 0) AS tickets_vendidos, -- Tickets vendidos se guarda en tickets_vendidos.
+COALESCE (SUM(l.numtickets), 0) - COALESCE(SUM(s.qtysold)) AS tickets_sin_vender -- diferencia entre tickets totales y tickets vendidos nos da tickets sin vender.
+FROM category c 
+LEFT JOIN event e ON e.catid = c.catid -- Se une evento con categoria la columna común es catid
+LEFT JOIN listing l ON l.eventid = e.eventid -- Se uen listing con evento la columna común es eventid
+LEFT JOIN sales s ON e.eventid = s.eventid -- Se une sales con event la columna comun es eventid.
+GROUP BY c.catgroup -- Los datos se agrupan por categoria de evento
+ORDER BY tickets_vendidos DESC; -- Los datos se ordenan por tickets vendidos. 
+
 -- Pregunta 2.19 Crea una consulta que calcule el precio promedio pagado por venta y la compare con el precio promedio por venta por trimestre. La consulta deberá mostrar tres campos: trimestre, precio_promedio_por_trimestre, precio_promedio_total
+SELECT 
+CONCAT(YEAR(s.saletime), 'Trimestre', QUARTER(s.saletime)) AS trimestre,
+ROUND(AVG(s.pricepaid), 2) AS precio_promedio_por_trimestre,
+(SELECT ROUND(AVG(pricepaid), 2) FROM sales) AS precio_promedio_total
+FROM sales s
+GROUP BY YEAR(s.saletime), QUARTER(s.saletime)
+ORDER BY YEAR(s.saletime), QUARTER(s.saletime);
+
 -- Pregunta 2.20 Muestra el total de tickets de entradas compradas de Shows y Conciertos.
+
+SELECT c.catgroup, -- 
+COALESCE (SUM(s.qtysold), 0) AS tickets_vendidos -- se obtiene la suma de todos los tickets vendidos.
+FROM category c 
+LEFT JOIN event e ON e.catid = c.catid -- Se une evento 
+LEFT JOIN sales s ON e.eventid = s.eventid
+WHERE c.catgroup IN ('Concerts', 'Shows')
+GROUP BY c.catgroup
+ORDER BY tickets_vendidos DESC;
+
 -- Pregunta 2.21 Muestra el id, fecha, nombre del evento y localización del evento que más entradas ha vendido.
+SELECT e.eventid, e.starttime, e.eventname, v.venuename, v.venuecity,
+SUM(s.qtysold) AS total_tickets_vendidos
+FROM event e
+JOIN venue v ON e.venueid = v.venueid
+JOIN sales s ON e.eventid = s.eventid
+GROUP BY e.eventid, e.starttime, e.eventname, v.venuename, v.venuecity
+ORDER BY total_tickets_vendidos DESC
+LIMIT 1;
 
 -- Resolver con Vistas
 
