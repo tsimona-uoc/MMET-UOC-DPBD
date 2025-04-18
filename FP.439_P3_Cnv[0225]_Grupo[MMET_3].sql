dea3a8a713f40c83ea0fb52919f32e70a3686e1b
rusterboy77 <37505980+rusterboy77@users.noreply.github.com>
2025-04-18 22:38:00 +0200

    Update FP.439_P3_Cnv[0225]_Grupo[MMET_3].sql

--- FP.439_P3_Cnv[0225]_Grupo[MMET_3].sql
+++ FP.439_P3_Cnv[0225]_Grupo[MMET_3].sql
@@ -36,22 +36,84 @@ ORDER BY d.week; /* Se ordena la salida por semana*/
 -- del usuario, de lo contrario es un visitante.
 -- Utilizar la función IF y agrupar.
 
-SELECT CONCAT(e.eventid, ' ', e.eventname) AS 'Evento', /*Concatenación de 'eventid' y 'eventname' para crear una columna que identifica los eventos de forma descriptiva.*/
-    SUM(IF(v.venuecity = u.city, 1, 0)) AS 'Asistentes Locales', /*Calcula el total de asistentes locales. Si la ciudad del lugar (venue) coincide con la ciudad del usuario, suma 1; de lo contrario, suma 0.*/
-    SUM(IF(v.venuecity <> u.city, 1, 0)) AS 'Visitantes' /*Calcula el total de asistentes visitantes. Si la ciudad del lugar es diferente de la ciudad del usuario, suma 1; de lo contrario, suma 0.*/
-FROM event e 
-JOIN venue v ON e.venueid = v.venueid /*Une la tabla de eventos con la tabla de lugares usando el 'venueid', que relaciona el evento con su lugar*/
-JOIN sales s ON e.eventid = s.eventid /*Une la tabla de eventos con la tabla de ventas para saber quién compró entradas para un evento específico*/
-JOIN users u ON s.buyerid = u.userid /*Une la tabla de ventas con la tabla de usuarios para acceder a la información de cada comprador (por ejemplo, su ciudad).*/
-JOIN date d ON e.dateid = d.dateid /*Une la tabla de eventos con la tabla de fechas para filtrar eventos según la semana específica.*/
-WHERE d.week = 9 /*Filtra los eventos que ocurrieron en la semana número 9.*/
-GROUP BY e.eventid, e.eventname /*Agrupa los resultados por cada evento, identificándolos mediante su ID y nombre.*/
-ORDER BY e.eventid; /*Ordena los resultados por 'eventid' para facilitar la lectura en orden ascendente.*/
-
+SELECT 
+	CONCAT(e.eventid, ' - ', e.eventname) AS Evento, /*Concateno las columnas para mostrar eventid y eventname juntos en la misma columna a la cual llamaré Evento */
+    SUM(IF(u.city = v.venuecity, 1, 0)) AS `Asistentes Locales`, /*Hago la suma de aquellos casos en los cuales el comprador es de la misma ciudad que el evento que se vaya a celebrar */
+    SUM(IF(u.city != v.venuecity, 1, 0)) AS `Asistentes Visitantes` /*Hago la suma de aquellos casos en los cuales el comprador es de ciudad diferente que el evento que se vaya a celebrar */
+FROM 
+	event e 
+    JOIN venue v ON e.venueid = v.venueid /*Combino la tabla venue con la tabla event donde venueid es PK en la tabla venue y FK en la tabla event */
+    JOIN sales s ON e.eventid = s.eventid /*Combino la tabla sales con la tabla event donde eventid es PK en la tabla event y FK en la tabla sales */
+    JOIN users u ON u.userid = s.buyerid /*Combino la tabla users con la tabla sales donde userid es PK en la tabla users y FK en la tabla sales*/
+    JOIN date d ON d.dateid = e.dateid /*Combino la tabla date con la tabla event donde dateid es PK en la tabla date y FK en la tabla event*/
+WHERE
+	d.week = 9 /*Añado la condición de la semana número nueve para solo considerar aquellos eventos celebrados esta semana*/
+GROUP BY
+	e.eventid, e.eventname /*Agrupo la consulta por eventid y eventname */
+ORDER BY e.eventid;
 
 -- Pregunta 3.11 Eliminar de la tabla users a todos aquellos usuarios registrados que no hayan comprado ni vendido 
 -- ninguna entrada. Antes de eliminarlos, copiarlos a una tabla denominada backup_users para poder recuperarlos en caso 
 -- de ser necesario.
+	
+/*Creación de tabla*/
+CREATE TABLE backup_users_test LIKE users; 
+
+START TRANSACTION;
+	
+/*Resolución del ejercicio con subconsultas*/
+INSERT INTO backup_users_test /*Se insertan en la tabla creada backup_users usuarios que cumplan las dos condiciones*/
+SELECT * FROM users
+WHERE userid NOT IN (SELECT sellerid FROM sales) AND userid NOT IN (SELECT buyerid FROM sales); /*userid no coincide con sellerid y userid no coincide con buyerid*/
+
+SET FOREIGN_KEY_CHECKS = 0; /*Se desactivan las restricciones de claves foráneas antes de eliminar usuarios, es también recomendable ejecutar START TRANSACTION para poder hacer ROLLBACK.*/
+    
+DELETE FROM users /*Se eliminan de la tabla users usuarios que cumplan las dos condiciones*/
+WHERE userid NOT IN (SELECT sellerid FROM sales) AND userid NOT IN (SELECT buyerid FROM sales); /*userid no coincide con sellerid y userid no coincide con buyerid*/
+
+COMMIT;
+    
+ROLLBACK;
+
+DROP TABLE backup_users_test;
+
+SELECT *
+FROM backup_users_test;
+
+SELECT *
+FROM users;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+/*Resolución del ejercicio con IF*/
+
+CREATE TABLE backup_users LIKE users;
+
+INSERT INTO backup_users /*Se insertan en la tabla creada backup_users usuarios que cumplan las dos condiciones*/
+SELECT * 
+FROM users
+WHERE
+	IF (userid NOT IN (SELECT sellerid FROM sales) AND userid NOT IN (SELECT buyerid FROM sales), 1, 0) = 1; /*userid no coincide con sellerid y userid no coincide con buyerid*/
+
+START TRANSACTION;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DELETE FROM users /*Se eliminan de la tabla users usuarios que cumplan las dos condiciones*/
+WHERE 
+	IF(userid NOT IN (SELECT sellerid FROM sales) AND userid NOT IN (SELECT buyerid FROM sales), 1, 0) = 1; /*userid no coincide con sellerid y userid no coincide con buyerid*/
+
+COMMIT;
+
+ROLLBACK;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+SELECT *
+FROM users;
+
+SELECT *
+FROM backup_users;
 
 -- Pregunta 3.12 Mostrar una lista de usuarios donde se especifique para cada usuario si éste es un comprador 
 -- (sólo ha comprado entradas), un vendedor (sólo ha vendido entradas) o ambos.
@@ -208,8 +270,44 @@ SELECT
 FROM users
 WHERE VIP = 'Sí' AND MONTH(birthdate) = MONTH(CURRENT_DATE()); -- Filtra solo usuarios VIP que cumplen años en el mes actual.
 
+-- 3.19 Inventar una función UDF que permita optimizar las operaciones de la Base de Datos. Justificarla.
+/*Se crea una subconsulta que devuelve la categoria musical más comprada por un usuario. 
+Esto puede ser útil a la hora de analizar el mercado para tener un mejor enfoque de negocio en la venta de entradas.*/
+DELIMITER // 
 
--- Pregunta 3.19 Inventar una función UDF que permita optimizar las operaciones de la Base de Datos. Justificarla.
+CREATE FUNCTION categoria_mas_comprada (uid INT) /*Se crea la función llamda categoria más vendida que recibirá un parámetro de tipo entero (id de usuario)*/
+RETURNS VARCHAR(10) /*La función devuelve el nombre de categoría, máximo 10 caracteres que es la misma longitud que tiene la columna catname en la tabla categoría*/
+DETERMINISTIC /*La función devolverá la misma salida para los mismos parámetros de entrada */
+READS SQL DATA /*La función es de solo lectura  ya que utiliza la consulta SELECT*/
+BEGIN
+	DECLARE mejor_categoria VARCHAR(10); /*Se declara la variable que almacenará el nombre de categoría */
+    SELECT c.catname /*Se extrae el nombre de la categoría de la tabla category y se inserta en la variable mejor_cateogria */
+    INTO mejor_categoria 
+    FROM sales s 
+    JOIN listing l ON s.listid = l.listid /*Combino la tabla sales con la tabla listing donde listid es PK en la tabla listing y FK en la tabla sales*/
+    JOIN event e ON l.eventid = e.eventid /*Combino la tabla listing con la tabla event donde eventid es PK en la tabla event y FK en la tabla listing */
+    JOIN category c ON e.catid = c.catid /*Combino la tabla event con la tabla category donde catid es PK de la tabla category y FK en la tabla event */
+    WHERE s.buyerid = uid /*Se filtan los registros para seleccionar el buyerid que coincida con buyerid recibido como parametro por la función*/
+    GROUP BY c.catname
+    ORDER BY COUNT(*) DESC
+    LIMIT 1;
+    
+    RETURN mejor_categoria;
+END//
+
+DELIMITER ;
+
+/*Para comprobar la función se crea la consulta con la tabla users, pasandole a la función el parámetro de userid*/
+SELECT 
+	u.userid,
+    u.username,
+    u.firstname,
+    u.lastname,
+    categoria_mas_comprada (u.userid) AS 'categoria preferida'
+FROM users u
+WHERE categoria_mas_comprada(u.userid) IS NOT NULL;
+
+DROP FUNCTION IF EXISTS `categoria_mas_comprada`;
 
 
 -- Variables de @usuario
@@ -246,3 +344,4 @@ set @monthbirthday = MONTH(curdate());
 SELECT userid, username, NombreResumido, VIP, dia, mes, birthdate
 FROM cumpleanhos
 WHERE VIP = @esVIP AND mes = @monthbirthday;
+
