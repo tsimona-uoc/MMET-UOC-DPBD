d9b29ab9fa67d3f1f2a9d3a7f390f0d14bf661ba
rusterboy77 <37505980+rusterboy77@users.noreply.github.com>
2025-04-30 13:51:18 +0200

    Create FP.439_P5_Cnv[0225]_Grupo[MMET_3].sql
    Creacion P5

---
+++ FP.439_P5_Cnv[0225]_Grupo[MMET_3].sql
@@ -0,0 +1,54 @@
+-- PLANTILLA DE ENTREGA DE LA PARTE PRÁCTICA DE LAS ACTIVIDADES
+-- --------------------------------------------------------------
+-- Actividad: AAn(P5)
+--
+-- Grupo: Cnv0225_Grupo03: [MMET]
+-- 
+-- Integrantes: 
+-- 1. Maria Ricart Martínez
+-- 2. Evarishtu Dongua Kuzin
+-- 3. Temis Simon Antonio
+-- 4. Marius Ciurana Rodríguez
+--
+-- Database: [fp_204_23]
+-- --------------------------------------------------------------
+-- 5.1 Crear manualmente (CREATE TABLE) una tabla denominada shows_semanales. Agregar los siguientes campos:
+  año: smallint
+  mes: char(5)
+  semana: smallint
+  catname: varchar(10)
+  eventname: varchar(200)
+  localidad: varchar(15)
+  starttime timestamp
+    
+-- 5.2 Crear un procedimiento almacenado denominado shows_semana_proxima que realice lo siguiente:
+  Vaciar la tabla shows_semanales.
+  Llenar la tabla con los shows planificados para la semana siguiente a la semana en curso. Llenar los campos con la siguiente información
+      año: Los cuatro dígitos del año (2008).
+      mes: Nombre del mes (abreviado), ejemplo: JUN.
+      semana: Número de semana, ejemplo: 26.
+      catname: Nombre descriptivo abreviado de un tipo de eventos en un grupo, ejemplo: Opera.
+      eventname: Nombre del evento, ejemplo: Hamlet.
+      localidad: Nombre del recinto, ejemplo: Cleveland Browns Stadium, concatenado con el nombre de la ciudad, ejemplo: Cleveland.
+      starttime Fecha y hora de inicio del evento, ejemplo: 2008-10-10 19:30:00.
+
+-- 5.3 Crear un evento que ejecute cada día sábado a las 8 de la mañana el procedimiento shows_semana_proxima y que permita exportar la tabla shows_semanales generada por el procedimiento anterior a un archivo de texto.
+    
+-- 5.4 Crear manualmente una tabla denominada ventas_entradas. Agregar los siguientes campos:
+  caldate: date. Fecha de calendario, como 2008-06-24.
+  sellerid: integer. Referencia de clave externa a la tabla USERS (el usuario que vendió los tickets).
+  sellername: varchar(35) Usar la función NombreResumido para llenar este campo
+  email: varchar(100) Dirección de correo electrónico del usuario
+  qtysold: integer. La cantidad de entradas vendidas en una fecha.
+  pricepaid: decimal(8,2) La suma del precio total por la venta de entradas.
+  profit: decimal(8,2) La suma de las ganancias 85% a pagar al vendedor para ese día.
+    
+-- 5.5 Crear un procedimiento almacenado denominado profit_sellers que realice lo siguiente:
+  Vaciar la tabla ventas_entradas
+  Llenar la tabla, para el día y mes que coincida con el día y el mes de la fecha actual (CURRENT_DATE()).
+  (Dado que el año es 2008 no se tomará en cuenta en el ejercicio).
+  La tabla deberá tener un registro por cada vendedor cuyas ventas de ese día sean superiores a 0.
+
+-- 5.6 Crear un evento que ejecute cada día a las 23:59 el procedimiento profit_sellers.
+
+-- 5.7 Inventar un procedimiento almacenado que permita optimizar las operaciones del sistema. Justificarlo
