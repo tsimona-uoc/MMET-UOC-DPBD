d8991a1ca594140855be20eb11136720762b97c1
maria-ricart <mricartmarti@uoc.edu>
2025-05-05 21:52:28 +0200

    Update FP.439_P5_Cnv[0225]_Grupo[MMET_3].sql

--- FP.439_P5_Cnv[0225]_Grupo[MMET_3].sql
+++ FP.439_P5_Cnv[0225]_Grupo[MMET_3].sql
@@ -20,6 +20,17 @@
   eventname: varchar(200)
   localidad: varchar(15)
   starttime timestamp
+
+   CREATE TABLE shows_semanales (
+    año smallint,
+    mes char(5),
+    semana smallint,
+    catname varchar(10),
+    eventname varchar(200),
+    localidad varchar(15),
+    starttime DATETIME
+  );
+  
     
 -- 5.2 Crear un procedimiento almacenado denominado shows_semana_proxima que realice lo siguiente:
   Vaciar la tabla shows_semanales.
@@ -31,6 +42,64 @@
       eventname: Nombre del evento, ejemplo: Hamlet.
       localidad: Nombre del recinto, ejemplo: Cleveland Browns Stadium, concatenado con el nombre de la ciudad, ejemplo: Cleveland.
       starttime Fecha y hora de inicio del evento, ejemplo: 2008-10-10 19:30:00.
+
+
+/*Primero creo una consulta para observar como se verá cada fila y de que tabla debo sacar cada información que pide el procedimiento*/
+
+SELECT 
+    YEAR(e.starttime) AS año,
+    UPPER(DATE_FORMAT(e.starttime, '%b')) AS mes,
+    WEEK(e.starttime, 1) AS semana,
+    c.catname,
+    e.eventname,
+    CONCAT(v.venuename, ', ', v.venuecity) AS localidad,
+    e.starttime
+FROM event e
+JOIN category c ON e.catid = c.catid
+JOIN venue v ON e.venueid = v.venueid
+LIMIT 10;
+
+/*Una vez observado que funciona, creamos el procedimiento teniendo en cuenta la semana próxima */
+
+DELIMITER //
+
+CREATE PROCEDURE shows_semana_proxima()
+BEGIN
+    TRUNCATE TABLE shows_semanales;
+
+    INSERT INTO shows_semanales (
+        año, mes, semana, catname, eventname, localidad, starttime
+    )
+    SELECT 
+        YEAR(e.starttime) AS año,
+        UPPER(DATE_FORMAT(e.starttime, '%b')) AS mes,
+        WEEK(e.starttime, 1) AS semana,
+        c.catname,
+        e.eventname,
+        CONCAT(v.venuename, ', ', v.venuecity) AS localidad,
+        e.starttime
+    FROM event e
+    JOIN category c ON e.catid = c.catid
+    JOIN venue v ON e.venueid = v.venueid
+    WHERE WEEK(e.starttime, 1) = WEEK(CURDATE(), 1) + 1
+      AND YEAR(e.starttime) = YEAR(CURDATE());
+END //
+
+SELECT 
+    YEAR(e.starttime) AS año,
+    UPPER(DATE_FORMAT(e.starttime, '%b')) AS mes,
+    WEEK(e.starttime, 1) AS semana,
+    c.catname,
+    e.eventname,
+    CONCAT(v.venuename, ', ', v.city) AS localidad,
+    e.starttime
+FROM event e
+JOIN category c ON e.catid = c.catid
+JOIN venue v ON e.venueid = v.venueid
+ORDER BY e.starttime
+LIMIT 20;
+
+DELIMITER ;
 
 -- 5.3 Crear un evento que ejecute cada día sábado a las 8 de la mañana el procedimiento shows_semana_proxima y que permita exportar la tabla shows_semanales generada por el procedimiento anterior a un archivo de texto.
     
